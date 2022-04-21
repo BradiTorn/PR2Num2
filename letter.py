3af5d6d99b375fcb3f8864be547f0d5d462636a1
BradiTorn <islam.kuliev123@gmail.com>
2022-04-22 02:32:45 +0300

    создание letter

---
+++ letter.py
@@ -0,0 +1,12 @@
+import os
+import math
+
+clear = lambda: os.system('cls')
+rez = 0
+
+def stroka(s):
+    clear()
+    print("Строка - " + str(s))
+    print("Длина введённой строки = " + str(len(s)))
+    print("Число пробелов = " + str(s.count(' ')))
+    print("Число запятых = " + str(s.count(',')))
